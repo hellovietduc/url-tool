43f47086f6eb6746d951421457b80f25fac874e4
Nguyễn Việt Đức <vietduc01100001@gmail.com>
2018-04-12 17:09:20 +0700

    Initialize project

---
+++ src/main/java/com/vieejtddwcs/urltool/App.java
@@ -0,0 +1,13 @@
+package com.vieejtddwcs.urltool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
